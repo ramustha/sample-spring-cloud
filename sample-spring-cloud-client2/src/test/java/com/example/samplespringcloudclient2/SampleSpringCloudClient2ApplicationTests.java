903f9f3568fe2e1434b41d2eccc5a2924da4d6b0
Ramadhani Musthofa <ramadhani.musthofa@gdn-commerce.com>
2022-06-08 11:29:42 +0700

    add Fallback on other service add FallbackHeaders filter

---
+++ sample-spring-cloud-client2/src/test/java/com/example/samplespringcloudclient2/SampleSpringCloudClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringcloudclient2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringCloudClient2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- sample-spring-cloud-gateway/src/main/java/com/example/samplespringcloudgateway/SampleSpringCloudGatewayApplication.java
+++ sample-spring-cloud-gateway/src/main/java/com/example/samplespringcloudgateway/SampleSpringCloudGatewayApplication.java
@@ -2,24 +2,12 @@ package com.example.samplespringcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SampleSpringCloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleSpringCloudGatewayApplication.class, args);
-	}
-
-}
-
-@RestController
-class FallbackController {
-	@GetMapping("/default-fallback")
-	Flux<Void> getDefaultFallback() {
-		return Flux.empty();
 	}
 
 }
