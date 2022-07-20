6716ecb47d592b08df7a36e39d1a06329a79a9c9
Ramadhani Musthofa <ramadhani.musthofa@gdn-commerce.com>
2022-07-20 10:09:00 +0700

    add gadget service

---
+++ gadget-service/src/main/java/com/example/samplespringcloudclient1/SampleSpringCloudGadgetServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.samplespringcloudclient1;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringCloudGadgetServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SampleSpringCloudGadgetServiceApplication.class, args);
+  }
+
+}
---
+++ gadget-service/src/test/java/com/example/samplespringcloudclient1/SampleSpringCloudGadgetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringcloudclient1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringCloudGadgetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
