a88272b9e4a743b083cc32bfbfae47f8f0b5ce75
Ramadhani Musthofa <ramadhani.musthofa@gdn-commerce.com>
2022-07-27 20:54:30 +0700

    add auth service with keycloak

--- product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -2,13 +2,27 @@ package com.example.productservice;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class ProductServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ProductServiceApplication.class, args);
+  }
+
+  @Bean
+  SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    return http.authorizeExchange(exchange -> exchange.matchers(EndpointRequest.toAnyEndpoint())
+            .permitAll()
+            .anyExchange()
+            .authenticated())
+        .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
+        .build();
   }
 
 }
